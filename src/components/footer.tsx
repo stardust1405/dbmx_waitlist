7aad4638ddc001f415d40ccbd946a0e3741f2320
Salim Rutaganda <rutagandasalim@gmail.com>
2025-05-17 18:19:42 +0200

    style: enhance footer component with separators and improved link styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,29 +1,35 @@
 import Link from "next/link";
 
 export default function Footer() {
-	return (
-		<footer className="flex flex-col justify-center items-center gap-4 pb-4">
-			<div className="flex flex-row justify-between">
-				<ul className="flex flex-row gap-4">
-					<li className="dark:text-muted-foreground dark:hover:text-foreground cursor-pointer">
-						Built using Next.js 15
-					</li>
-					<li className="dark:text-muted-foreground dark:hover:text-foreground">
-						<Link href="https://github.com/idee8/nextjs-notion-waitlist">
-							Get this template
-						</Link>
-					</li>
-					<li className="dark:text-muted-foreground dark:hover:text-foreground cursor-pointer">
-						<Link href="https://kofi.com/idee8">Become a Sponsor</Link>
-					</li>
-				</ul>
-			</div>
-			<div>
-				<p className="text-sm text-muted-foreground">
-					&copy; {new Date().getFullYear()} Crafted by{" "}
-					<Link href="https://idee8.agency">Idee8</Link>
-				</p>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="flex flex-col justify-center items-center gap-4 pb-4">
+      <div className="flex flex-row justify-between">
+        <ul className="flex flex-row gap-4">
+          <li className="dark:text-muted-foreground dark:hover:text-foreground cursor-pointer">
+            Built using Next.js 15
+          </li>
+          <li className="dark:text-muted-foreground dark:hover:text-foreground">
+            •
+          </li>
+          <li className="dark:text-muted-foreground dark:hover:text-foreground">
+            <Link href="https://github.com/idee8/nextjs-notion-waitlist">
+              Get this template
+            </Link>
+          </li>
+          <li className="dark:text-muted-foreground dark:hover:text-foreground">
+            •
+          </li>
+          <li className="dark:text-muted-foreground dark:hover:text-foreground cursor-pointer">
+            <Link href="https://kofi.com/idee8">Become a Sponsor</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p className="text-sm text-muted-foreground">
+          &copy; {new Date().getFullYear()} Crafted by{" "}
+          <Link href="https://idee8.agency" className="font-semibold text-foreground">Idee8</Link>
+        </p>
+      </div>
+    </footer>
+  );
 }
