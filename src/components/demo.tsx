59db5229e4138072f575f75a6d92dc8eb25d9b2c
Karan Singhasane <ksinghasane14@gmail.com>
2025-10-17 18:50:08 +0530

    fix alignment for video

--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -18,25 +18,25 @@ export default function Demo({
 }: DemoProps) {
 
 	return (
-		<div className="py-14 sm:px-0 px-4">
-			<div className="bg-muted/40 rounded-lg p-2 max-w-3xl mx-auto">
+		<div className="py-14 sm:px-0">
+			<div className="bg-muted/40 rounded-xl w-184 pb-2 mx-auto">
 				<div className="flex flex-row justify-start items-center gap-2 p-2">
-					<span className="relative flex h-4 w-4">
-						<span className="absolute inline-flex h-full w-full rounded-full bg-background opacity-75" />
+					<span className="relative flex h-3 w-3">
+						<span className="absolute inline-flex h-full w-full rounded-full bg-background bg-red-500" />
 					</span>
-					<span className="relative flex h-4 w-4">
-						<span className="absolute inline-flex h-full w-full rounded-full bg-background opacity-75" />
+					<span className="relative flex h-3 w-3">
+						<span className="absolute inline-flex h-full w-full rounded-full bg-background bg-yellow-500" />
 					</span>
-					<span className="relative flex h-4 w-4">
-						<span className="absolute inline-flex h-full w-full rounded-full bg-background opacity-75" />
+					<span className="relative flex h-3 w-3">
+						<span className="absolute inline-flex h-full w-full rounded-full bg-background bg-green-500" />
 					</span>
 				</div>
 				<div
-					className={`relative w-full max-w-3xl mx-auto rounded-lg overflow-hidden ${className}`}
+					className={`relative w-180 h-full mx-auto rounded-lg overflow-hidden ${className}`}
 				>
 					{/* Glitch effect container */}
-					<div className="relative w-full aspect-video bg-black rounded-lg overflow-hidden">
-						<YouTubeEmbed videoid="Djh-9sSi68c" params="controls=1,autoplay=1" />
+					<div className="aspect-video w-full h-full bg-black justify-center items-center rounded-lg overflow-hidden">
+						<YouTubeEmbed height={1000} videoid="Djh-9sSi68c" params="controls=1,autoplay=1" />
 					</div>
 				</div>
 
