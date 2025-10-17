a82d6ca9993dc641b2a87c85af12fa0073ddc05f
Karan Singhasane <ksinghasane14@gmail.com>
2025-10-17 18:25:12 +0530

    fix alignment and remove modal for video

--- src/components/powered.tsx
+++ src/components/powered.tsx
@@ -1,4 +1,4 @@
-import { NextjsLogo, NotionLogo, ResendLogo, UpstashLogo, GoLogo } from "./svgs";
+import { NextjsLogo, NotionLogo, ResendLogo, UpstashLogo } from "./svgs";
 import { VercelLogo } from "./svgs";
 
 export default function Powered() {
@@ -6,11 +6,11 @@ export default function Powered() {
 		<div className="flex flex-col items-center justify-center gap-12 py-12">
 			<div className="flex flex-col items-center justify-center gap-2">
 				<h3 className="text-foreground text-2xl font-semibold">Tech Stack</h3>
-				<p className="text-muted-foreground text-base">
+				<p className="text-muted-foreground text-base text-center">
 					Blazing fast Desktop App that helps you iterate faster. Built with -
 				</p>
 			</div>
-			<div className="flex items-center sm:gap-12 gap-6">
+			<div className="flex items-center justify-center flex-wrap sm:gap-12 gap-6">
 				<img
 					src="https://upload.wikimedia.org/wikipedia/commons/0/05/Go_Logo_Blue.svg"
 					alt="Go Logo"
